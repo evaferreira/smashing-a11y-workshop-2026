b74de2fc58436211bdb8e733e5e6179d58be6288
Jung, Paul <paul.jung@dm.de>
2026-09-10 14:15:10 +0200

    wip

--- src/atoms/Button/IconOnly.stories.tsx
+++ src/atoms/Button/IconOnly.stories.tsx
@@ -5,6 +5,11 @@ import { Button } from './Button';
 const meta = {
   title: 'Broken/Icon Button',
   component: Button,
+  parameters: {
+    a11y: {
+      test: "todo"
+    }
+  }
 } satisfies Meta<typeof Button>;
 
 export default meta;
